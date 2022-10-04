21658a44ed930dc42f6d565bce3339278e77bb3e
Anthony Sarmiento <113078785+YAGURAMI@users.noreply.github.com>
2022-10-04 10:01:44 +0800

    Add files via upload

---
+++ Basic Input Operators.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//Basic Initializer Operators, Computation 
+class Main {
+   
+
+public static void main(String[] args) {
+        
+Scanner sc = new Scanner(System.in);
+        
+        
+System.out.print("Enter 1st number: ");
+       
+int num1 = sc.nextInt();
+        
+System.out.print("Enter 2nd number: ");
+        
+int num2 = sc.nextInt();
+        
+        
+
+System.out.println("______________________"+"\n"+"Sum: "+(num1+num2)+"\n"+"Product: "+(num1*num2)+"\n"+"Subtrahend: "+(num1-num2)+"\n"+"Division: "+(num1/num2)+"\n"+"Remainder: "+(num1%num2));
+    
+}
+
+}
